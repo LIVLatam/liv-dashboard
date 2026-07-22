360712fe28b3d6edf23f53b04f6c2718acb21476
LIVLatam <lucas@inteligenciadevida.com>
2026-07-22 18:38:55 -0300

    csf.js: alta CSF Salvador Novo

--- csf.js
+++ csf.js
@@ -1,2 +1,2 @@
 /* CSF · generado por el barrido de la carpeta de Drive. Colegio con carpeta = compartió. */
-window.LIV_CSF={"updated":"22/07/2026 13:49 CDMX","si":[2, 4, 5, 6, 7, 8, 9, 11, 14, 15, 17, 20, 21, 22, 25, 28, 29, 30, 32, 33, 35, 43, 45, 46, 56]};
+window.LIV_CSF={"updated":"22/07/2026 15:37 CDMX","si":[2, 4, 5, 6, 7, 8, 9, 11, 14, 15, 17, 20, 21, 22, 23, 25, 28, 29, 30, 32, 33, 35, 43, 45, 46, 56]};
